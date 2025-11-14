55ceba32706bac7ae61f3e7ca14339d6086af6cb
HoTuanKhai <khaihtgcd210244@fpt.edu.vn>
2025-11-15 06:07:59 +0700

    Fix axios to deploy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+	baseURL: BASE_URL,
 	withCredentials: true, // send cookies to the server
 });
 
