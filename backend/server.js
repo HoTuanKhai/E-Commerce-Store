bd45ea769f525891294b33c8114f0637fb9e66da
HoTuanKhai <khaihtgcd210244@fpt.edu.vn>
2025-11-17 18:01:16 +0700

    fix to deploy on Render

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path"
 
 //routes
 import authRoutes from "./routes/auth.route.js"
@@ -18,8 +19,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
